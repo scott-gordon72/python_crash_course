ebb17fcb5a473f84669c940e8a5319a4b8db2787
scott <scott.gordon72@outlook.com>
2020-04-16 12:16:53 -0400

    Update my_cars.py by importing entire module and access class using dot notation

--- chapter_9/my_cars.py
+++ chapter_9/my_cars.py
@@ -1,7 +1,7 @@
-from car import Car, ElectricCar
+import car
 
-my_beetle = Car('volkswagon', 'beetle', 2019)
+my_beetle = car.Car('volkswagon', 'beetle', 2019)
 print(my_beetle.get_descriptive_name())
 
-my_tesla = ElectricCar('tesla', 'roadster', 2019)
+my_tesla = car.ElectricCar('tesla', 'roadster', 2019)
 print(my_tesla.get_descriptive_name())
