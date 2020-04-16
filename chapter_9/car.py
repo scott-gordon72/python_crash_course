13aadd488c3e2db60e1d5403675b4622f86d1b9d
scott <scott.gordon72@outlook.com>
2020-04-16 12:30:08 -0400

    Remove ElectricCar and Battery classes from car.py

--- chapter_9/car.py
+++ chapter_9/car.py
@@ -1,4 +1,4 @@
-"""A set of classes used to represent gas and electric cars."""
+"""A class that can be used to represent a car."""
 
 
 class Car:
@@ -35,36 +35,3 @@ class Car:
             self.odometer_reading += miles
         else:
             print("You can't roll back an odometer")
-
-
-class Battery:
-    """A simple attempt to model a battery for an electric car."""
-
-    def __init__(self, battery_size=75):
-        """Initialize the battery's attributes."""
-        self.battery_size = battery_size
-
-    def describe_battery(self):
-        """Print a statement describing the battery size."""
-        print(f"This car has a {self.battery_size}-kWh battery.")
-
-    def get_range(self):
-        """Print a statement about the range this battery provides."""
-        if self.battery_size == 75:
-            range = 260
-        elif self.battery_size == 100:
-            range = 315
-
-        print(f"This car can go about {range} miles on a full charge.")
-
-
-class ElectricCar(Car):
-    """Models aspects of a car, specific to electric vehicles."""
-
-    def __init__(self, make, model, year):
-        """
-        Initialize attributes of the parent class.
-        Then initialize attributes specific to an electric car.
-        """
-        super().__init__(make, model, year)
-        self.battery = Battery()
