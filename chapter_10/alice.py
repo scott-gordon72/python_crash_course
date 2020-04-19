d16a475ca0e100df3470650fa34313d0a5014678
scott <scott.gordon72@outlook.com>
2020-04-19 14:03:07 -0400

    Add alice.py and alice.txt

--- chapter_10/alice.py
+++ chapter_10/alice.py
@@ -1,7 +1,12 @@
-filename = 'alice.txt'
+filename = 'chapter_10/text_files/alice.txt'
 
 try:
     with open(filename, encoding='utf-8') as f:
         contents = f.read()
 except FileNotFoundError:
     print(f"Sorry, this file {filename} does not exist.")
+else:
+    # Count the appropriate number of words in the file.
+    words = contents.split()
+    num_words = len(words)
+    print(f"The file {filename} has about {num_words} words.")
